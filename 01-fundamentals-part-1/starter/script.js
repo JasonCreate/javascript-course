35359283b9299bad6d5a0f19cc73b70c0cfd2e4e
JasonBalasbas <markjason.balasbas@gmail.com>
2025-09-02 09:58:08 +0800

    feat: add Part 2 starter files and documentation

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -361,5 +361,19 @@ if (age >= 18) {
 }
 console.log(drink2);
 
-
 console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+
+/////////////////////////////////////////
+//Coding Challenge #4
+
+const bill = 275; 
+
+// Step 1: Create the tip calculation using ternary operator
+//Rule: 15% if bill is between 50 and 300, otherwise 20%
+//Hint: bill >=50 && bill <= 300
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+//Step 2: Create beautiful output with template literal 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
