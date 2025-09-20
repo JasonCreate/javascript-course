dd3cbdc30992593703851830599bc9e9e62692c4
JasonBalasbas <markjason.balasbas@gmail.com>
2025-09-20 22:29:17 +0800

    t(modal): implement modal open/close functionality with DOM manipulation and helper functions

--- 05-modal/starter/script.js
+++ 05-modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// modal elements
+const modalEl = document.querySelector('.modal');
+
+// overlay modal
+const overlayEl = document.querySelector('.overlay');
+
+// close modal button
+const btnCloseModalEl = document.querySelector('.close-modal');
+
+//open modal
+const btnsOpenModalEl = document.querySelectorAll('.show-modal');
+
+console.log('Open buttons:', btnsOpenModalEl.length);
+
+const openModal = function () {
+  // remove hidden classname from modal
+  modalEl.classList.remove('hidden');
+
+  // remove the hidden keyword classname in overlay
+  overlayEl.classList.remove('hidden');
+};
+
+btnsOpenModalEl.forEach(btn => btn.addEventListener('click', openModal));
